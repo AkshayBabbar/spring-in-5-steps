cd7b5d3bbc905f76ace3af9f152ab6230745e9f6
akshaybabbar12@gmail.com <akshaybabbar12@gmail.com>
2019-02-10 01:10:04 +0530

    removing Override

--- src/main/java/com/babbarEnterprises/spring/basics/springin5steps/QuickSortAlgorithm.java
+++ src/main/java/com/babbarEnterprises/spring/basics/springin5steps/QuickSortAlgorithm.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class QuickSortAlgorithm implements SortAlgorithm {
-
-    @Override
     public  int[] sort(int[] numbers){
 //      QuickSort Numbers;
         return numbers;
