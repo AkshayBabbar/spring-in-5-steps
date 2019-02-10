fd06a54798a3be5fb0b6ecaeb97c0af3cae6090c
akshaybabbar12@gmail.com <akshaybabbar12@gmail.com>
2019-02-11 04:59:54 +0530

    Updated till step 17

--- src/main/java/com/babbarEnterprises/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ src/main/java/com/babbarEnterprises/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -1,14 +1,21 @@
 package com.babbarEnterprises.spring.basics.springin5steps.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class BinarySearchImpl {
+
+    private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     @Qualifier("bubble")
@@ -18,5 +25,15 @@ public class BinarySearchImpl {
 
         int[] sortedNumbers = sortAlgorithm.sort(numbers);
         return 3;
+    }
+
+    @PostConstruct
+    public void postContruct() {
+        LOGGER.info("POST --- CONTRUCT");
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        LOGGER.info(" ~~~~PRE --- DESTROY");
     }
 }
