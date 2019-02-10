6b29afbba335fba169afd5219d65685817e77021
akshaybabbar12@gmail.com <akshaybabbar12@gmail.com>
2019-02-10 18:48:42 +0530

    Updated till step 15

--- src/main/java/com/babbarEnterprises/spring/basics/springin5steps/scope/JdbcConnection.java
+++ src/main/java/com/babbarEnterprises/spring/basics/springin5steps/scope/JdbcConnection.java
@@ -1,8 +1,11 @@
 package com.babbarEnterprises.spring.basics.springin5steps.scope;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class JdbcConnection {
     public JdbcConnection() {
         System.out.println("JDBC CONNECTION");
